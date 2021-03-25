489739e1b70ed661dd0bf2b044106fedc97d2f94
Gabriel Lisboa da Silva <gabriellisboa08@gmail.com>
2021-03-25 20:12:35 -0300

    Criação do mini-formulario
    Praticando sozinho

---
+++ index.js
@@ -0,0 +1,15 @@
+window.alert('Por Favor Preencha os dados apenas com a verdade!')
+function Confirmar(){
+    let txtn = document.getElementById('txtn')
+    let txti = document.getElementById('txti')
+    let txtc = document.getElementById('txtc')
+    let txts = document.getElementById('txts')
+    let nome = String(txtn.value)
+    let idade = Number(txti.value)
+    let cargo = String(txtc.value)
+    let salario = Number(txts.value)
+    document.write(`Seu nome é ${nome}, tem ${idade} anos, trabalha atualmente de ${cargo}, recebendo ${salario} reais`)
+
+}
+        
+  
