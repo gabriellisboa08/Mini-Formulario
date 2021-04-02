1734515439f39f30c31db718f1fa97f6b0b7ac6f
Gabriel Lisboa da Silva <gabriellisboa08@gmail.com>
2021-04-01 21:51:17 -0300

    alterando formatação dos códigos
    melhorando um pouco o código

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ function Confirmar(){
     let cargo = String(txtc.value)
     let salario = Number(txts.value)
     document.write(`Seu nome é ${nome.toUpperCase()}, tem ${idade} anos, atualmente trabalha como ${cargo}, recebendo um salário de ${salario.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})}`)
-
 }
         
   
